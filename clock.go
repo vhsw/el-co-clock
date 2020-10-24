cab905ad8cb98d9fe7af0c22e0222135b145367d
Alexey Dubrov <vhsw@ya.ru>
2020-10-25 00:31:20 +0300

    editable table

--- clock.go
+++ clock.go
@@ -1,37 +1,88 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"time"
 
 	"github.com/gotk3/gotk3/gtk"
 )
 
-func update(store *gtk.ListStore, n *int) {
+type item struct {
+	id      *gtk.TreeIter
+	time    string
+	message string
+}
+
+func update() {
 	for {
 		time.Sleep(1 * time.Second)
+		fmt.Println(store.IterNChildren(nil))
 	}
 }
 
+var (
+	store *gtk.ListStore
+)
+
+func onEnableToggled(cell *gtk.CellRendererToggle, pathString string) {
+	it, _ := store.GetIterFromString(pathString)
+	valueObj, _ := store.GetValue(it, 1)
+	value, _ := valueObj.GoValue()
+	bValue, _ := value.(bool)
+	store.SetValue(it, 1, !bValue)
+}
+
+func onTimeEdited(cell *gtk.CellRendererText, pathString string, text string) {
+	it, _ := store.GetIterFromString(pathString)
+	store.SetValue(it, 2, text)
+}
+
+func onMessageEdited(cell *gtk.CellRendererText, pathString string, text string) {
+	it, _ := store.GetIterFromString(pathString)
+	store.SetValue(it, 3, text)
+}
+
 func main() {
-	// Initialize GTK without parsing any command line arguments.
 	gtk.Init(nil)
-
-	builder, _ := gtk.BuilderNew()
-
-	builder.AddFromFile("clock.glade")
-
-	// treeObj, _ := builder.GetObject("tree")
-	// tree := treeObj.(*gtk.TreeView)
-
+	builder, _ := gtk.BuilderNewFromFile("clock.glade")
+	signals := map[string]interface{}{
+		"on_enable_toggled": onEnableToggled,
+		"on_time_edited":    onTimeEdited,
+		"on_message_edited": onMessageEdited,
+	}
+	builder.ConnectSignals(signals)
 	storeObj, _ := builder.GetObject("liststore")
-	store := storeObj.(*gtk.ListStore)
+	store = storeObj.(*gtk.ListStore)
 	addObj, _ := builder.GetObject("add")
 	addButton := addObj.(*gtk.Button)
-	n := 1
 	addButton.Connect("clicked", func() {
 		it := store.Append()
-		n++
-		store.Set(it, []int{0, 1}, []interface{}{1, true})
+		store.Set(it, []int{0, 1, 2, 3}, []interface{}{1, true, "", ""})
+	})
+	removeObj, _ := builder.GetObject("remove")
+	removeButton := removeObj.(*gtk.Button)
+	selectObj, _ := builder.GetObject("selection")
+	selection := selectObj.(*gtk.TreeSelection)
+	var selectedPath *gtk.TreePath
+	selection.Connect("changed", func() {
+		var iter *gtk.TreeIter
+		var model gtk.ITreeModel
+		var ok bool
+		model, iter, ok = selection.GetSelected()
+		if ok {
+			removeButton.SetSensitive(true)
+			path, err := model.(*gtk.TreeModel).GetPath(iter)
+			if err != nil {
+				log.Printf("treeSelectionChangedCB: Could not get path from model: %s\n", err)
+				return
+			}
+			selectedPath = path
+		}
+	})
+	removeButton.Connect("clicked", func() {
+		it, _ := store.GetIter(selectedPath)
+		store.Remove(it)
 	})
 	winObj, _ := builder.GetObject("main")
 	win := winObj.(*gtk.Window)
@@ -39,6 +90,6 @@ func main() {
 		gtk.MainQuit()
 	})
 	win.ShowAll()
-	go update(store, &n)
+	go update()
 	gtk.Main()
 }
